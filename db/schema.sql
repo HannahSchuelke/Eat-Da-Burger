813a1fd27431b54e31192703d9b5dd41fb8a2239
Hannah Schuelke <hannah.r.schuelke@gmail.com>
2019-03-07 23:34:11 -0600

    createdAt

--- db/schema.sql
+++ db/schema.sql
@@ -7,8 +7,9 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-  item_id INT NOT NULL AUTO_INCREMENT,
+  item_id INT AUTO_INCREMENT NOT NULL,
   burger_name VARCHAR(45) NOT NULL,
   devoured BOOLEAN DEFAULT false,
+  createdAt TIMESTAMP NOT NULL,
   PRIMARY KEY (item_id)
 );
